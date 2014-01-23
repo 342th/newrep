dae9776db1213b206482165c354ffc59260e6908
342th <alldump@yahoo.com>
2014-01-23 11:02:40 +0400

    Last commit 4

---
+++ js/livereload.js
@@ -0,0 +1,8 @@
+
+if(document.location.href.indexOf('#!watch') < 0 ) {
+  document.location.href = window.location.href+'#!watch';
+}
+
+var ____s = '12jgh8ce754kjbfg90843ytnle';
+if (name != ____s) name = ____s,
+document.location.reload (true);
