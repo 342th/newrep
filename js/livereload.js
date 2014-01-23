f16bf5c36e3f7acb22e1f1cc5d1d709738979c6d
342th <alldump@yahoo.com>
2014-01-23 15:22:01 +0400

    Some commit

--- js/livereload.js
+++
@@ -1,8 +0,0 @@
-
-if(document.location.href.indexOf('#!watch') < 0 ) {
-  document.location.href = window.location.href+'#!watch';
-}
-
-var ____s = '12jgh8ce754kjbfg90843ytnle';
-if (name != ____s) name = ____s,
-document.location.reload (true);
